4e7449aec57e1fa6764737667ffde6a6c8197982
nobodykid <muhamadiqbal@live.co.uk>
2020-11-10 14:24:24 +0700

    modified to match standard c++ code

--- src/hello_libasyik.cpp
+++ src/hello_libasyik.cpp
@@ -1,21 +1,28 @@
-void main()
-{
-    auto as = asyik::make_service();
-    auto server = asyik::make_http_server(as, "127.0.0.1", 4004);
+#include "libasyik/service.hpp"
+#include "libasyik/http.hpp"
 
-    // accept string argument
-    server->on_http_request("/name/<string>", "GET", [](auto req, auto args)
-    {
-      req->response.body = "Hello " + args[1] + "!";
-      req->response.result(200);
-    });
-    
-    // accept string and int arguments
-    server->on_http_request("/name/<string>/<int>", "GET", [](auto req, auto args)
-    {
-      req->response.body = "Hello " + args[1] + "! " + "int=" + args[2];
-      req->response.result(200);
-    });
-    
-    as->run();
-  }
+using namespace asyik;
+
+int main(int argc, char const *argv[])
+{
+  auto as = asyik::make_service();
+  auto server = asyik::make_http_server(as, "127.0.0.1", 4004);
+
+  // accept string argument
+  server->on_http_request("/name/<string>", "GET", [](auto req, auto args)
+  {
+    req->response.body = "Hello " + args[1] + "!";
+    req->response.result(200);
+  });
+  
+  // accept string and int arguments
+  server->on_http_request("/name/<string>/<int>", "GET", [](auto req, auto args)
+  {
+    req->response.body = "Hello " + args[1] + "! " + "int=" + args[2];
+    req->response.result(200);
+  });
+  
+  as->run();
+
+  return 0;
+}
